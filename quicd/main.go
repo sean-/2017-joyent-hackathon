2b81e27a903ff3cade276cd7dabe213745cd6a05
Frank Schroeder <frank.schroeder@gmail.com>
2017-11-15 13:25:22 -0800

    quicd: add QUIC support

--- quicd/main.go
+++ quicd/main.go
@@ -5,28 +5,48 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/lucas-clemente/quic-go/h2quic"
 )
 
 func main() {
 	log.SetFlags(0)
 
 	var addr, upstream string
+	var keyFile, certFile string
 	flag.StringVar(&addr, "addr", ":8080", "host:port listen address")
 	flag.StringVar(&upstream, "upstream", "", "host:port of upstream server")
+	flag.StringVar(&keyFile, "key", "", "TLS key file")
+	flag.StringVar(&certFile, "cert", "", "TLS cert file")
 	flag.Parse()
 
 	if upstream == "" {
 		log.Fatal("no upstream address")
 	}
 
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	log.Print("Starting quicd")
+	log.Print("Forwarding to ", upstream)
+
+	handle := func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.RequestURI)
 		httputil.NewSingleHostReverseProxy(r.URL).ServeHTTP(w, r)
-	}))
-
-	log.Print("Listening on ", addr)
-	log.Print("Forwarding to ", upstream)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatal(err)
 	}
+	http.Handle("/", http.HandlerFunc(handle))
+
+	go func() {
+		log.Print("Listening HTTP on ", addr)
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		log.Print("Listening QUIC on ", addr)
+		if err := h2quic.ListenAndServeQUIC(addr, certFile, keyFile, nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	log.Print("CTRL-C to exit")
+	select {}
 }
