33e985f898827132c92c1d8de288fd13c1cd8f07
Sean Chittenden <sean@chittenden.org>
2017-11-15 13:51:47 -0800

    Add metrics support for timing information

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Move along, nothing to see here.
 
 ## Items
 
-[X] Setup http reverse proxy - Frank
-[ ] Integrate some form of monitoring to establish a baseline - Sean
-[ ] Add QUIC server support - Frank
+[X] Setup http reverse proxy
+[X] Integrate some form of monitoring to establish a baseline
+[X] Add HTTPS server support
+[X] Add QUIC server support
 [X] Add Makefile to cross-compile
 [ ] Add QUIC client support
 [ ] Add node QUIC support - Trent
 [ ] Change imgapi to talk to local proxy - Trent
 [ ] Change proxy to talk to upstream manta - Trent
+[ ] Add tests
--- quicd/main.go
+++ quicd/main.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"flag"
-	"log"
+	stdlog "log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"time"
 
 	"github.com/lucas-clemente/quic-go/h2quic"
+	"github.com/sean-/seed"
 )
 
+var logger *stdlog.Logger
+
 func main() {
+	log := stdlog.New(os.Stdout, "", stdlog.LstdFlags)
 	log.SetFlags(0)
+	logger = log
 
 	var addr, addrtls, upstream string
 	var keyFile, certFile string
+	var circonusAPIKey string
+
 	flag.StringVar(&addr, "addr", ":8080", "host:port for HTTP")
 	flag.StringVar(&addr, "addrtls", ":8443", "host:port for HTTPS")
+	flag.StringVar(&addr, "addr", ":8080", "host:port listen address")
 	flag.StringVar(&upstream, "upstream", "", "http://host:port/ of upstream server")
 	flag.StringVar(&keyFile, "key", "", "TLS key file")
 	flag.StringVar(&certFile, "cert", "", "TLS cert file")
+	flag.StringVar(&circonusAPIKey, "circonus-api-key", "", "Circonus API Key")
+
 	flag.Parse()
 
 	if upstream == "" {
@@ -31,10 +43,21 @@ func main() {
 		log.Fatal("Invalid upstream URL: ", err)
 	}
 
+	if err := initMetrics(circonusAPIKey); err != nil {
+		log.Fatalf("Unable to initialize metrics: %v", err)
+		os.Exit(1)
+	}
+
 	log.Print("Starting quicd")
 	log.Print("Forwarding to ", upstreamURL)
 
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		const requestLatency = "request-latency"
+		defer func() {
+			metrics.Timing(requestLatency, float64(time.Now().Sub(start)))
+		}()
+
 		log.Printf("%s %s", r.Method, r.RequestURI)
 		httputil.NewSingleHostReverseProxy(upstreamURL).ServeHTTP(w, r)
 	}))
@@ -71,4 +94,8 @@ func main() {
 
 	log.Print("CTRL-C to exit")
 	select {}
+}
+
+func init() {
+	seed.MustInit()
 }
