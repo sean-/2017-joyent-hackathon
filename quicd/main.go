dcc4a71a5e2b4851e35ce0f4b19127e7a9254749
Sean Chittenden <sean@chittenden.org>
2017-11-15 14:16:24 -0800

    Add cross-compile metadata for goreleaser.

--- quicd/main.go
+++ quicd/main.go
@@ -7,13 +7,18 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	cgm "github.com/circonus-labs/circonus-gometrics"
 	"github.com/lucas-clemente/quic-go/h2quic"
+	"github.com/pkg/errors"
 	"github.com/sean-/seed"
 )
 
 var logger *stdlog.Logger
+var metrics *cgm.CirconusMetrics
 
 func main() {
 	log := stdlog.New(os.Stdout, "", stdlog.LstdFlags)
@@ -102,4 +107,42 @@ func main() {
 
 func init() {
 	seed.MustInit()
+}
+
+func initMetrics(circonusAPIKey string) (err error) {
+	log := logger
+	cmc := &cgm.Config{}
+	cmc.Debug = false // set to true for debug messages
+	cmc.Log = log
+
+	cmc.CheckManager.API.TokenApp = "quicd"
+
+	// Circonus API Token key (https://login.circonus.com/user/tokens)
+	if circonusAPIKey == "" {
+		cmc.CheckManager.API.TokenKey = os.Getenv("CIRCONUS_API_TOKEN")
+	} else {
+		cmc.CheckManager.API.TokenKey = circonusAPIKey
+	}
+
+	log.Println("Creating new cgm instance")
+
+	metrics, err = cgm.NewCirconusMetrics(cmc)
+	if err != nil {
+		logger.Println(err)
+		return errors.Wrap(err, "unable to initialize metrics")
+	}
+
+	logger.Println("Adding ctrl-c trap")
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		logger.Println("Received CTRL-C, flushing outstanding metrics before exit")
+		metrics.Flush()
+		os.Exit(0)
+	}()
+
+	logger.Println("Starting to send metrics")
+
+	return nil
 }
